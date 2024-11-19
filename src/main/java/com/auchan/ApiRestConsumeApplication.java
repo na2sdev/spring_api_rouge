0869edcff0a8b7cfd5359c8b87bcd137143687dd
monacodev <nassim.chabane@gmail.com>
2024-11-19 12:01:14 +0100

    ossature projet

---
+++ src/main/java/com/auchan/ApiRestConsumeApplication.java
@@ -0,0 +1,13 @@
+package com.auchan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestConsumeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestConsumeApplication.class, args);
+	}
+
+}
